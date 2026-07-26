c196d66520fa7c90948bd657b19513ee5f166a06
nahuelmararrabeia <nahuelmm5@gmail.com>
2026-07-25 23:07:34 -0300

    add health check

--- backend/Restaurant.Api/Program.cs
+++ backend/Restaurant.Api/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddHealthChecks();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,5 +26,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseHealthChecks("/health");
 
 app.Run();
